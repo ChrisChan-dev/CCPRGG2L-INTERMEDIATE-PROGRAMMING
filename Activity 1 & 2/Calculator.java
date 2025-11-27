c68b69c2f521271492f94157bf852753ed28c4a6
Christian <johnchristianlolos160@gmail.com>
2025-11-27 09:00:57 +0800

    Add files via upload

---
+++ Activity 1 & 2/Calculator.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
---
+++ Activity 1 & 2/PersonalInformation.java
@@ -0,0 +1,38 @@
+package com.mycompany.personalinformation;
+
+public class PersonalInformation {
+
+    public static void main(String[] args) {
+        
+        String name = "John Christian B. Lolos";
+        int age = 18;
+        
+        String Edu1 = "Kindergarten (2012 - 2013)";
+        String Edu2 = "Elementary (2013 - 2019)";
+        String Edu3 = "Junior High (2019 - 2023)";
+        String Edu4 = "Senior High (2023 - 2025)";
+        String Edu5 = "College (2025 - Present)";
+        
+        String hobby = "Rubik's Cubing";
+        String achievements = "NASA Space Apps Challenge : GLOBAL NOMINEE";
+        
+        System.out.println("Name:");
+        System.out.println("- " + name);
+        
+        System.out.println("Age:");
+        System.out.println("- " + age);
+        
+        System.out.println("Educational Background:");
+        System.out.println("- " + Edu1);
+        System.out.println("- " + Edu2);
+        System.out.println("- " + Edu3);
+        System.out.println("- " + Edu4);
+        System.out.println("- " + Edu5);
+        
+        System.out.println("Hobbies:");  
+        System.out.println("- " + hobby);
+        
+        System.out.println("Achievements:");
+        System.out.println("- " + achievements);
+    }
+}
