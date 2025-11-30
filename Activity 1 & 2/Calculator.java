63607ef6a77faf24cc7073609bda4fdc553ccab4
Christian <johnchristianlolos160@gmail.com>
2025-11-30 17:12:09 +0800

    Refactor Calculator to BasicCalculator with new operations

--- Activity 1 & 2/Calculator.java
+++ Activity 1 & 2/Calculator.java
@@ -1,13 +1,18 @@
-
-package com.mycompany.calculator;
-
-public class Calculator {
-
+public class BasicCalculator {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
         
-        System.out.println("Addition: " + (a + b));
-        System.out.println("Modulus: " + (a % b));
+        double num1 = 20;
+        double num2 = 5;
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println("Numbers: " + num1 + " and " + num2);
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
     }
 }
